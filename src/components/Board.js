9cbd1936764fe1d1422e173b4f15cd429b45fa33
origin <piwe2004@gmail.com>
2023-04-25 16:33:00 +0900

    X,O 변경 및 승자 출력

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,12 +4,41 @@ import "./Board.css"
 
 const Board = () => {
 
-    const [squares, setSquares] = useState(Array(9).fill(null))
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(true);
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        for(let i = 0; i< lines.length; i++){
+            const [a,b,c] = lines[i]
+            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
+        return null;
+    }
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status = 'Winner: ' + winner;
+    }else{
+        status = 'Next Player:'+ (xIsNext? 'X' : 'O');
+    }
 
     const handleClick = (i) => {
         const newSquares = squares.slice();
-        newSquares[i] = 'X'
-        setSquares(newSquares)
+        if(calculateWinner(newSquares) || newSquares[i]) return;
+        newSquares[i] = xIsNext ? 'X' : 'O';
+        setSquares(newSquares);
+        setXIsNext(!xIsNext);
         
     }
 
@@ -17,9 +46,11 @@ const Board = () => {
         return <Square value={squares[i]} onClick={()=>handleClick(i)} />
     }
 
+
+
     return (
         <div>
-            <div className='status'>Next Player: X, O</div>
+            <div className='status'>{status}</div>
             <div className='board_row'>
                 {rnderSquart(0)}
                 {rnderSquart(1)}
