c3ca2d0b62e2009d087b501c6c6c6600c078756e
origin <piwe2004@gmail.com>
2023-04-25 17:15:14 +0900

    히스토리 작업

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,73 +1,32 @@
-import React, { useState } from 'react'
-import Square from './Square'
-import "./Board.css"
+import React, { useState } from "react";
+import Square from "./Square";
+import "./Board.css";
 
-const Board = () => {
+const Board = ({status, onClick}) => {
+    
+  const rnderSquart = (i) => {
+    return <Square value={status[i]} onClick={()=>onClick(i)} />;
+  };
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-    const calculateWinner = (squares) => {
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-        for(let i = 0; i< lines.length; i++){
-            const [a,b,c] = lines[i]
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a];
-            }
-        }
-        return null;
-    }
-    const winner = calculateWinner(squares);
-    let status;
-    if(winner){
-        status = 'Winner: ' + winner;
-    }else{
-        status = 'Next Player:'+ (xIsNext? 'X' : 'O');
-    }
+  return (
+    <div>
+      <div className="board_row">
+        {rnderSquart(0)}
+        {rnderSquart(1)}
+        {rnderSquart(2)}
+      </div>
+      <div className="board_row">
+        {rnderSquart(3)}
+        {rnderSquart(4)}
+        {rnderSquart(5)}
+      </div>
+      <div className="board_row">
+        {rnderSquart(6)}
+        {rnderSquart(7)}
+        {rnderSquart(8)}
+      </div>
+    </div>
+  );
+};
 
-    const handleClick = (i) => {
-        const newSquares = squares.slice();
-        if(calculateWinner(newSquares) || newSquares[i]) return;
-        newSquares[i] = xIsNext ? 'X' : 'O';
-        setSquares(newSquares);
-        setXIsNext(!xIsNext);
-        
-    }
-
-    const rnderSquart = (i) => {
-        return <Square value={squares[i]} onClick={()=>handleClick(i)} />
-    }
-
-
-
-    return (
-        <div>
-            <div className='status'>{status}</div>
-            <div className='board_row'>
-                {rnderSquart(0)}
-                {rnderSquart(1)}
-                {rnderSquart(2)}
-            </div>
-            <div className='board_row'>
-                {rnderSquart(3)}
-                {rnderSquart(4)}
-                {rnderSquart(5)}
-            </div>
-            <div className='board_row'>
-                {rnderSquart(6)}
-                {rnderSquart(7)}
-                {rnderSquart(8)}
-            </div>
-        </div>
-    )
-}
-
-export default Board
+export default Board;
